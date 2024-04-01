6f534f43d6f4e3c0d37618e6131917e43478d5ac
Dang Gia Kiet <danggiakiet2803@gmail.com>
2024-04-02 00:41:58 +0700

    cap nhap

--- Kiểm tra trắc nghiệm/Kiểm tra trắc nghiệm/loadData.cs
+++ Kiểm tra trắc nghiệm/Kiểm tra trắc nghiệm/loadData.cs
@@ -1,4 +1,5 @@
-﻿using OfficeOpenXml;
+﻿using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,11 +28,11 @@ namespace Kiểm_tra_trắc_nghiệm
                     {
                         // Khởi tạo các biến để chứa các giá trị lấy từ dữ liệu theo từng dòng
                         string cauHoi = Convert.ToString(worksheet.Cells[i, 1].Value ?? "");
-                        string cauA = Convert.ToString(worksheet.Cells[i, 3].Value ?? "");
-                        string cauB = Convert.ToString(worksheet.Cells[i, 4].Value ?? "");
-                        string cauC = Convert.ToString(worksheet.Cells[i, 5].Value ?? "");
-                        string cauD = Convert.ToString(worksheet.Cells[i, 6].Value ?? "");
-                        int dapAnDung = Convert.ToInt32(worksheet.Cells[i, 7].Value ?? "");
+                        string cauA = Convert.ToString(worksheet.Cells[i, 2].Value ?? "");
+                        string cauB = Convert.ToString(worksheet.Cells[i, 3].Value ?? "");
+                        string cauC = Convert.ToString(worksheet.Cells[i, 4].Value ?? "");
+                        string cauD = Convert.ToString(worksheet.Cells[i, 5].Value ?? "");
+                        int dapAnDung = Convert.ToInt32(worksheet.Cells[i, 6].Value ?? "");
                         // Tạo biến cauHoi để thêm vào dsCauHoi
                         cauHoi cauHoii = new cauHoi(cauHoi, cauA, cauB, cauC, cauD, dapAnDung);
                         // Thêm biến vừa tạo vào dsNhanVien
@@ -43,8 +44,7 @@ namespace Kiểm_tra_trắc_nghiệm
             {
                 // Thông báo lỗi
                 MessageBox.Show("Lỗi khi đọc dữ liệu từ Excel: " + ex.Message, "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-            }
+                return;            }
         }
     }
 }
